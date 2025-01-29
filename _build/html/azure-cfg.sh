ece7b524f0d842ab9a790d54314d2b9b56067caa
Ehsan <eh.etesami@gmail.com>
2025-01-29 16:13:20 +0000

    Create statis configs and helm chart folders. Use curl to apply configs.

---
+++ _build/html/azure-cfg.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [[ ! -f $AZURE_CONFIG_PATH ]]; then
+  echo "Azure config file not found at $AZURE_CONFIG_PATH"
+  exit 1
+fi
+
+cont_enc=$(cat $AZURE_CONFIG_PATH | base64 -w0)
+
+cat <<EOF | kubectl apply -f -
+apiVersion: azure.upbound.io/v1beta1
+metadata:
+  name: provider-cfg-azure
+  labels:
+    skycluster.io/managed-by: skycluster
+kind: ProviderConfig
+spec:
+  credentials:
+    source: Secret
+    secretRef:
+      namespace: crossplane-system
+      name: secret-azure
+      key: configs
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: secret-azure
+  namespace: skycluster
+type: Opaque
+data:
+  configs: $cont_enc
+EOF
