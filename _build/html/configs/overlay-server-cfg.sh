c01128b1241654180b1154676312bf20093f9d26
Ehsan <eh.etesami@gmail.com>
2025-02-06 00:34:16 +0000

    Connect google analytics

---
+++ _build/html/configs/overlay-server-cfg.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ -z "$HOST" ]] || [[ -z "$TOKEN" ]] || [[ -z "$PORT" ]] || [[ -z $CA_CERTIFICATE ]]; then
+  echo "HOST, TOKEN, PORT and CA_CERTIFICATE must be set."
+  exit 1
+fi
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  namespace: skycluster
+  name: overlay-server-cfg
+  labels:
+    skycluster.io/managed-by: skycluster
+    skycluster.io/secret-type: overlay-server
+type: Opaque
+stringData:
+  config: |
+    {
+      "host": "https://$HOST",
+      "port": "$PORT",
+      "token": "$TOKEN",
+      "ca_cert": "$(cat $CA_CERTIFICATE | base64 -w0)"
+    }
+EOF
