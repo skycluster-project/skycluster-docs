11fb3918d1c6838960ad77c7fcc822b85f24b19c
etesami <etesami@users.noreply.github.com>
2025-09-04 02:12:51 +0000

    deploy: 2c483f5b3924c4835cde06670b74f40cad3500c4

---
+++ latest/configs/tailscale-cron.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+
+HEADSCALE_DATA=$(kubectl get secret headscale-connection-secret \
+  -n skycluster-system -o jsonpath='{.data}')
+if [[ -z "$HEADSCALE_DATA" ]]; then
+  echo "Error: Headscale data not found in headscale-connection-secret secret" >&2
+  exit 1
+fi
+
+# KEY
+HEADSCALE_KEY=$(echo "$HEADSCALE_DATA" | jq -r '."preauth.json"' | base64 -d | jq -r '.key')
+if [[ -z "$HEADSCALE_KEY" ]]; then
+  echo "Error: Headscale key not found in headscale-connection-secret secret" >&2
+  exit 1
+fi
+
+# TAILSCALE IP
+SERVER="https://$(curl -s ifconfig.io):8080"
+sudo tailscale up --login-server $SERVER --auth-key $HEADSCALE_KEY --accept-routes
+
